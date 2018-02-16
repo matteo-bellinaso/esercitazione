7e39eacff14082c72262ffc9587601b7d81cfb25
Matteo Bellinaso <matteobellinaso@MacBook-Pro-di-Matteo.local>
2018-02-16 10:02:36 +0100

    games list added with IMG

--- esercitazione/src/app/list/list.component.ts
+++ esercitazione/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { listItem } from './listItem';
 
 @Component({
   selector: 'list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+
+  games : listItem[] = [
+
+    new listItem("../assets/cod.jpg","Call Of Duty","sparatutto","49.99"),
+    new listItem("../assets/shadow.png","Shadow of the Colossus","Avventura Fantasy","29.99"),
+    new listItem("../assets/batman.jpg","Batman Arkham Knight","avventura","39.99"),
+    new listItem("../assets/crash.png","Crash Bandicoot","avventura - platform","29.99")
+
+  ]
 
   constructor() { }
 
---
+++ esercitazione/src/app/list/listItem.ts
@@ -0,0 +1,15 @@
+export class listItem{
+
+    id : string;
+    name: string;
+    description: string;
+    price : string;
+
+constructor(id : string, name: string, description : string, price: string){
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+}
+
+}
